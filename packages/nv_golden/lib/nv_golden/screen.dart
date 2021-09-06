8fd57bb7b2dbe8af6cfdd3781139a00fded0eecb
Marco Papula <m.papula@gmx.at>
2021-09-06 16:36:18 +0200

    add test devices

--- packages/nv_golden/lib/nv_golden/screen.dart
+++ packages/nv_golden/lib/nv_golden/screen.dart
@@ -23,14 +23,29 @@ class Screen {
 }
 
 /// Some common devices pre-created. We highly recommend including the iphone 5s
-/// since it's one of the smallest screens still in circulation.
+/// since it's one of the smallest screens still in circulation. All our test
+/// devices have their devicePixelRatio set to 1.0 for performance reasons.
 abstract class Device {
   static Screen get iphone5s => Screen(
         name: 'Iphone 5S',
         size: Size(320, 568),
       );
+
   static Screen get iphone12pro => Screen(
         name: 'Iphone 12 Pro',
         size: Size(390, 844),
+        safeArea: const EdgeInsets.fromLTRB(0.0, 47.0, 0.0, 34.0),
+      );
+
+  static Screen get iphone12proMax => Screen(
+        name: 'Iphone 12 Pro',
+        size: Size(428, 926),
+        safeArea: const EdgeInsets.fromLTRB(0.0, 47.0, 0.0, 34.0),
+      );
+
+  static Screen get huaweiP9 => Screen(
+        name: 'Huawei P9',
+        size: Size(360, 598),
+        safeArea: const EdgeInsets.fromLTRB(0.0, 24.0, 0.0, 0.0),
       );
 }
