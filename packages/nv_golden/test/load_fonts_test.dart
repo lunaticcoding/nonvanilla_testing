97130520eb0b1b6e46a76ed6e714b8b1a76de98f
Marco Papula <m.papula@gmx.at>
2021-11-10 13:41:17 +0100

    test custom font loader

---
+++ packages/nv_golden/test/load_fonts_test.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:nv_golden/nv_golden.dart';
+import 'package:nv_golden/nv_golden/nv_golden_builder.dart';
+
+import 'nonvanilla_testing.dart';
+import 'sample_widgets/icon_button.dart';
+
+void main() {
+  setUpAll(() async {
+    await NvGolden.loadFont(
+      name: 'Roboto',
+      paths: ['lib/fonts/Roboto-Regular.ttf'],
+    );
+  });
+
+  testWidgets('test widget with custom font loader in 2x2 grid',
+      (tester) async {
+    final nvWrapper = NvWidgetWrapper()..withDirectionality();
+    final smallDevice = Screen(size: Size(150, 60));
+
+    final nvGolden = NvGolden.grid(nrColumns: 2, screen: smallDevice)
+      ..addScenario(
+        name: 'Icon 1',
+        widget: nvWrapper.wrap(
+          SampleIconButton(text: 'icon 1', icon: Icons.title),
+        ),
+      )
+      ..addScenario(
+        name: 'Icon 2',
+        widget: nvWrapper.wrap(
+          SampleIconButton(text: 'icon 2', icon: Icons.build),
+        ),
+      )
+      ..addScenario(
+        name: 'Icon 3',
+        widget: nvWrapper.wrap(
+          SampleIconButton(text: 'icon 3', icon: Icons.expand),
+        ),
+      )
+      ..addScenario(
+        name: 'Icon 4',
+        widget: nvWrapper.wrap(
+          SampleIconButton(text: 'icon 4', icon: Icons.circle),
+        ),
+      );
+
+    await tester.createGolden(nvGolden, 'custom_font_icon_button');
+  });
+}
