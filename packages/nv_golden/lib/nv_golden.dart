0f5b870c5b588a4e011996480cb844abef0ec8ba
Marco Papula <m.papula@gmx.at>
2022-02-10 01:10:25 +0100

    refactor nv_golden interface to include singular goldens

--- packages/nv_golden/lib/nv_golden.dart
+++ packages/nv_golden/lib/nv_golden.dart
@@ -1,5 +1,65 @@
 library nv_golden;
 
-export 'nv_golden/nv_golden_builder.dart'
-    show NvWidgetWrapper, NvGolden, CreateGolden;
+import 'package:flutter/widgets.dart';
+import 'package:nv_golden/nv_golden/loading/font_loader.dart';
+import 'package:nv_golden/nv_golden/nv_golden_multiple.dart';
+import 'package:nv_golden/nv_golden/nv_golden_singular.dart';
+import 'package:nv_golden/nv_golden/screen.dart';
+
+export 'nv_golden/create_golden.dart';
+export 'nv_golden/nv_golden_multiple.dart';
 export 'nv_golden/screen.dart' show Device, Screen;
+
+abstract class NvGolden {
+  static NvGoldenMultiple grid({
+    required int nrColumns,
+    Screen? screen,
+    Widget Function(Widget child)? wrap,
+    Widget Function(Widget child)? wrapScenario,
+    BoxDecoration? decoration,
+  }) =>
+      NvGoldenMultiple.grid(
+        nrColumns: nrColumns,
+        screen: screen,
+        wrap: wrap,
+        wrapScenario: wrapScenario,
+        decoration: decoration,
+      );
+
+  static NvGoldenMultiple devices({
+    required List<Screen> deviceSizes,
+    Widget Function(Widget child)? wrap,
+    Widget Function(Widget child)? wrapScenario,
+    BoxDecoration? decoration,
+  }) =>
+      NvGoldenMultiple.devices(
+        wrap: wrap,
+        wrapScenario: wrapScenario,
+        decoration: decoration,
+        deviceSizes: deviceSizes,
+      );
+
+  static NvGoldenSingular singular({
+    required Widget widget,
+    required Screen screen,
+  }) =>
+      NvGoldenSingular(
+        widget: widget,
+        screen: screen,
+      );
+
+  /// Initialize golden tests
+  static Future<void> init() => loadAppFonts();
+
+  /// Optionally load fonts
+  static Future<void> loadFont({
+    required String name,
+    required List<String> paths,
+    bool isRelativePath = true,
+  }) =>
+      loadCustomFont(
+        name: name,
+        paths: paths,
+        isRelativePath: isRelativePath,
+      );
+}
