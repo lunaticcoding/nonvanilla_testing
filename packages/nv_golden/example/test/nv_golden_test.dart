1d53f6806c2f309751a0c157bf9dcf939254e233
lunaticcoding <jydnar-vYnjik-vomfu8>
2022-08-23 15:11:23 +0200

    update tests

--- packages/nv_golden/example/test/nv_golden_test.dart
+++ packages/nv_golden/example/test/nv_golden_test.dart
@@ -1,36 +1,46 @@
+import 'package:example/components/delete_icon.dart';
 import 'package:example/details_page.dart';
-import 'package:example/main.dart';
+import 'package:example/main_page.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:nv_golden/nv_golden.dart';
-import 'package:nv_golden/nv_golden/loading/font_loader.dart';
+import 'package:nv_golden/nv_golden/models/nv_gesture.dart';
 
 import 'nonvanilla_testing.dart';
 
 void main() {
-  setUpAll(loadAppFonts);
+  setUpAll(NvGolden.init);
 
   group('Devices', () {
     testWidgets('check devices - Main Page', (tester) async {
+      final wrapper = NvWrapper()..withMaterialApp();
+
       final nvGolden = NvGolden.devices(deviceSizes: [
         Device.iphone5s,
         Device.iphone12pro,
+        Device.iphone12proMax,
       ])
         ..addScenario(
           name: 'Title',
-          widget: MyApp(),
+          widget: wrapper.wrap(MainPage()),
         );
       await tester.createGolden(nvGolden, 'devices/main_page');
     });
 
     testWidgets('check devices - Details Page', (tester) async {
+      final wrapper = NvWrapper()..withMaterialApp();
+
       final nvGolden = NvGolden.devices(deviceSizes: [
         Device.iphone5s,
         Device.iphone12pro,
+        Device.iphone12proMax,
       ])
         ..addScenario(
           name: 'Title',
-          widget: DetailsPage(
-            entry: Entry(text: 'Test text', state: CheckState.inProgress),
+          widget: wrapper.wrap(
+            DetailsPage(
+              entry: Entry(text: 'Test text', state: CheckState.inProgress),
+            ),
           ),
         );
       await tester.createGolden(nvGolden, 'devices/details_page');
@@ -38,22 +48,54 @@ void main() {
   });
 
   group('Interaction', () {
-    testWidgets('Details Page', (tester) async {
-      final nvWrapper = NvWidgetWrapper()..withDirectionality();
+    testWidgets('Main Page', (tester) async {
+      final nvGolden = NvGolden.singular(
+        screen: Device.iphone12pro,
+        widget: MainPage(),
+      );
 
-      final nvGolden = NvGolden.(deviceSizes: [
-        Device.iphone5s,
-        Device.iphone12pro,
-      ])
-        ..addScenario(
-          name: 'Title',
-          widget: nvWrapper.wrap(
-            DetailsPage(
-              entry: Entry(text: 'Test text', state: CheckState.inProgress),
-            ),
+      await tester.createSequenceGolden(
+        nvGolden,
+        'interaction/main_page_delete/1',
+        gestures: [
+          NvGestureTap(
+            finder: () => find.byType(DeleteIcon).at(1),
+            apply: true,
           ),
-        );
-      await tester.createGolden(nvGolden, 'interaction/details_page');
+        ],
+      );
+
+      await tester.createSequenceGolden(
+        nvGolden,
+        'interaction/main_page_delete/2',
+        gestures: [],
+      );
+    });
+
+    testWidgets('Details Page', (tester) async {
+      final nvGolden = NvGolden.singular(
+        screen: Device.iphone12pro,
+        widget: DetailsPage(
+          entry: Entry(text: 'Test text', state: CheckState.inProgress),
+        ),
+      );
+
+      await tester.createSequenceGolden(
+        nvGolden,
+        'interaction/details_page_drag/1',
+        gestures: [
+          NvGestureDrag(
+            finder: () => find.byType(Image).at(1),
+            offset: Offset(-100, 0),
+          ),
+        ],
+      );
+
+      await tester.createSequenceGolden(
+        nvGolden,
+        'interaction/details_page_drag/2',
+        gestures: [],
+      );
     });
   });
 }
