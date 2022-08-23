ba44ee2bd7e74ab8dcb61102ce8e3d10554cc899
lunaticcoding <jydnar-vYnjik-vomfu8>
2022-08-23 15:11:08 +0200

    add with material app wrapper

--- packages/nv_golden/example/test/nonvanilla_testing.dart
+++ packages/nv_golden/example/test/nonvanilla_testing.dart
@@ -1,11 +1,20 @@
+import 'package:example/main.dart';
 import 'package:flutter/material.dart';
 import 'package:nv_golden/nv_golden.dart';
 
-extension ShortCuts on NvWidgetWrapper {
+class NvWrapper extends NvWidgetWrapper {
   void withDirectionality({TextDirection? textDirection}) => add(
         (child) => Directionality(
           textDirection: textDirection ?? TextDirection.ltr,
           child: child,
+        ),
+      );
+
+  void withMaterialApp() => add(
+        (child) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: lightTheme,
+          home: child,
         ),
       );
 }
