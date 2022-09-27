e5e7137fa05f67029831337a377a270a345c3a73
lunaticcoding <jydnar-vYnjik-vomfu8>
2022-09-27 18:04:01 +0200

    fix rect calculation

--- packages/nv_golden/lib/nv_golden/image_drawers/drag_drawer.dart
+++ packages/nv_golden/lib/nv_golden/image_drawers/drag_drawer.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:image/image.dart';
+import 'package:nv_golden/nv_golden/image_drawers/custom_rect_drawer.dart';
 
 void drawDrag(
   Image image,
@@ -13,11 +14,26 @@ void drawDrag(
   final white = getColor(255, 255, 255);
   final grey = getColor(200, 200, 200);
 
-  final alpha = asin((y0 - y1).abs() / (x0 - x1).abs());
-  final yd = (size * cos(alpha)).round();
-  final xd = (size * sin(alpha)).round();
+  final xV = x0 - x1;
+  final yV = y0 - y1;
 
-  fillRect(image, x0 + xd, y0 + yd, x1 - xd, y1 - yd, grey);
+  final xd =
+      sqrt((pow(20, 2) * pow(yV, 2)) / (pow(xV, 2) + pow(yV, 2))).round();
+  final yd = yV == 0 ? size : (xV * xd / (-yV)).round();
+
+  fillCustomRect(
+    image,
+    x0 + xd,
+    y0 + yd,
+    x1 + xd,
+    y1 + yd,
+    x1 - xd,
+    y1 - yd,
+    x0 - xd,
+    y0 - yd,
+    grey,
+  );
+
   fillCircle(image, x1, y1, size, grey);
   fillCircle(image, x0, y0, size, white);
 }
